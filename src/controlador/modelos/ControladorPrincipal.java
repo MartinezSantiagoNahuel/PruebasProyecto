3fa00509ea9aabb0b573b5b3fcaab84821c6fb46
gabinete <estudiante@gabinete.com>
2023-09-19 16:39:46 -0300

    tp1 terminado

--- src/controlador/modelos/ControladorPrincipal.java
+++ src/controlador/modelos/ControladorPrincipal.java
@@ -4,35 +4,107 @@
  */
 package controlador.modelos;
 
+import java.util.ArrayList;
 import usuarios.modelos.Clientes;
 import usuarios.modelos.Empleados;
 import usuarios.modelos.Encargados;
+import usuarios.modelos.Productos;
+
+import usuarios.modelos.Usuario;
 
 /**
  *
  * @author estudiante
  */
-public class ControladorPrincipal {
-    public static void main (String[] args){
+public class ControladorPrincipal
+{
+    
+    public static void main (String[] args)
+    {
+        
+        ArrayList<Clientes> clientes = new ArrayList<>();
+        ArrayList<Empleados> empleados = new ArrayList<>();
+        ArrayList<Encargados> encargados = new ArrayList<>();
+        ArrayList<Productos> productos = new ArrayList<>();
+        ArrayList<Usuario> usuario = new ArrayList<>();
+        
         Clientes cliente1 = new Clientes("c1@gmail.com","c1","c1","c1");
         Clientes cliente2 = new Clientes("c2@gmail.com","c2","c2","c2");
         Clientes cliente3 = new Clientes("c3@gmail.com","c3","c3","c3");
-        Clientes cliente4 = new Clientes("c4@gmail.com","c4","c4","c4");
-        Clientes cliente5 = new Clientes("c5@gmail.com","c5","c5","c5");
         
         Empleados empleado1 = new Empleados("em1@gmail.com","em1","em1","em1");
         Empleados empleado2 = new Empleados("em2@gmail.com","em2","em2","em2");
         Empleados empleado3 = new Empleados("em3@gmail.com","em3","em3","em3");
-        Empleados empleado4 = new Empleados("em4@gmail.com","em4","em4","em4");
-        Empleados empleado5 = new Empleados("em5@gmail.com","em5","em5","em5");
-        
+
         Encargados encargado1 = new Encargados("en1@gmail.com","en1","en1","en1");
         Encargados encargado2 = new Encargados("en1@gmail.com","en1","en1","en1");
         Encargados encargado3 = new Encargados("en1@gmail.com","en1","en1","en1");
-        Encargados encargado4 = new Encargados("en1@gmail.com","en1","en1","en1");
-        Encargados encargado5 = new Encargados("en1@gmail.com","en1","en1","en1");
+
+        Productos producto1 = new Productos("p1","Disponible",123,"p1",1.2f,"Postre");
+        Productos producto2 = new Productos("p2","No disponible",123,"p2",1.2f,"Postre");
+        Productos producto3 = new Productos("p3","Disponible",123,"p3",1.2f,"Postre");
         
-        //FALTA PRODUCTOS
+        clientes.add(cliente1);
+        clientes.add(cliente2);
+        clientes.add(cliente2);
+        
+        empleados.add(empleado1);
+        empleados.add(empleado2);
+        empleados.add(empleado3);
+        
+        encargados.add(encargado1);
+        encargados.add(encargado2);
+        encargados.add(encargado2);
+        
+        productos.add(producto1);
+        productos.add(producto2);
+        productos.add(producto3);
+        
+        
+        for (Clientes unCliente : clientes) 
+        {
+            
+            System.out.println(unCliente.verNombre());
+            System.out.println(unCliente.verApellido());
+            System.out.println(unCliente.verCorreo());
+            System.out.println(unCliente.verClave());
+            
+        }
+        
+        for (Empleados unEmpleado : empleados) 
+        {
+            
+            System.out.println(unEmpleado.verNombre());
+            System.out.println(unEmpleado.verApellido());
+            System.out.println(unEmpleado.verCorreo());
+            System.out.println(unEmpleado.verClave());
+            
+            
+        }
+        
+        for (Encargados unEncargado : encargados) 
+        {
+            
+            System.out.println(unEncargado.verNombre());
+            System.out.println(unEncargado.verApellido());
+            System.out.println(unEncargado.verCorreo());
+            System.out.println(unEncargado.verClave());
+            
+            
+        }
+        
+        for (Productos unProducto : productos) 
+        {
+            
+            System.out.println(unProducto.verNombre());
+            System.out.println(unProducto.verEstado());
+            System.out.println(unProducto.verDescripcion());
+            System.out.println(unProducto.verEstado());
+            System.out.println(unProducto.verCategoria());
+            System.out.println(unProducto.verPrecio());
+            
+        }
+        
     }
 }
     
--- src/usuarios/modelos/Productos.java
+++ src/usuarios/modelos/Productos.java
@@ -10,14 +10,26 @@ package usuarios.modelos;
  */
 public class Productos {
     //Atributos
-    private String nombre;
-    private String estado;
+    public String nombre;
+    public String estado;
+    private int codigo;
+    private String descripcion;
+    private float precio;
+    private String categoria;
+    
+    //cod
     
     //Constructor
-    public Productos(String nombre, String estado) {
+
+    public Productos(String nombre, String estado, int codigo, String descripcion, float precio, String categoria) {
         this.nombre = nombre;
         this.estado = estado;
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.categoria = categoria;
     }
+    
     
     //Metodos
     //Metodos GET/SET
@@ -35,6 +47,38 @@ public class Productos {
 
     public void asignarEstado(String estado) {
         this.estado = estado;
+    }
+
+    public int verCodigo() {
+        return codigo;
+    }
+
+    public void asignarCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String verDescripcion() {
+        return descripcion;
+    }
+
+    public void asignarDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float verPrecio() {
+        return precio;
+    }
+
+    public void asignarPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String verCategoria() {
+        return categoria;
+    }
+
+    public void asignarCategoria(String categoria) {
+        this.categoria = categoria;
     }
 
 }
